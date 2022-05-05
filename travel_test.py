471ee80fa1d49f0601d59801f251c2d0b8821c16
Saqib-Haider <saqibhaider567@gmail.com>
2022-05-05 14:07:24 +0600

    Added travel test

--- travel_test.py
+++ travel_test.py
@@ -1,6 +1,16 @@
 from travel import Space
-from nose.tools import *
+from numpy import testing as npt
 
 def test_space():
     mars = Space("Mars", "This is a red planet, with very less water supply")
-    assert_equal(mars.name, "Mars")
+    npt.assert_equal(mars.name, "Mars")
+    npt.assert_equal(mars.path, [])
+
+def test_travel_path():
+    middle = Space("Middle", "You are in middle of space")
+    up = Space("Up", "You went upwards direction")
+    down = Space("down", "You went downwards direction")
+
+    middle.add_path['up': up, 'down': down]
+    npt.assert_equal(middle.travel('up'), up)
+    npt.assert_equal(middle.travel('down'), down)
